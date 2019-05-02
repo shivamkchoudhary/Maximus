d74d4a20c461bca35b659b93bcf2a77aea209d87
Prakasaka - the illuminator <44729591+Prakasaka@users.noreply.github.com>
2019-05-02 21:06:33 +0530

    Update schd.py

--- userbot/modules/schd.py
+++ userbot/modules/schd.py
@@ -10,11 +10,11 @@ async def _(event):
         return
     input_str = event.pattern_match.group(1)
     ttl = 0
-    message = "SYNTAX: `.schd <time_in_seconds> ;=; <message to send>`"
+    message = "SYNTAX: `.schd <time_in_seconds> - <message to send>`"
     if input_str:
         await event.delete()
         if ";=;" in input_str:
-            ttl, message = input_str.split(";=;")
+            ttl, message = input_str.split("-")
         elif event.reply_to_msg_id:
             await event.delete()
             ttl = int(input_str)
